cdf20a4637af1aa0274d008adce2ab7f7ba5099d
Djeessy <56890614+Djeessy@users.noreply.github.com>
2019-12-09 21:41:18 -0300

    Implementado primeiro caso de teste automatizado

--- src/__test__/Users.test.js
+++ src/__test__/Users.test.js
@@ -1,22 +1,19 @@
-import React from 'react';
-import { store } from '../components/util';
-import { mount } from 'enzyme';
-import App from '../App';
-import 'jsdom-global/register';
-import jsdom from 'jsdom'
-const doc = new jsdom.JSDOM('<!doctype html><html><body></body></html>')
-global.document = doc
-global.window = doc.defaultView
+import configureStore from 'redux-mock-store';
+import users from '../assets/users.json';
+const mockStore = configureStore([]);
 
 describe('Test the users list', () => {
+    let store = mockStore({
+        users: users
+    });
+
     it('Update list index 0 to firstName = Eren lastName = Yeager', () => {
-        mount(<App/>)
-        // let users = store.getState().users
-        // expect(users[0].firstName === 'John').toBeTruthy();
-        // expect(users[0].lastName === 'Doe').toBeTruthy();
-        // users[0].firstName = 'Eren';
-        // users[0].lastName = 'Yeager';
-        // expect(users[0].firstName === 'Eren').toBeTruthy();
-        // expect(users[0].lastName === 'Yeager').toBeTruthy();
+        let users = store.getState().users
+        expect(users[0].firstName === 'John').toBeTruthy();
+        expect(users[0].lastName === 'Doe').toBeTruthy();
+        users[0].firstName = 'Eren';
+        users[0].lastName = 'Yeager';
+        expect(users[0].firstName === 'Eren').toBeTruthy();
+        expect(users[0].lastName === 'Yeager').toBeTruthy();
     });
 });
